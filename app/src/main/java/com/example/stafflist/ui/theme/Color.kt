c6909970c250c08d2af337a239e6bc6d77986b57
Danya Goga <CryingSakura69@gmail.com>
2024-03-10 14:41:39 +0200

    add colors

--- app/src/main/java/com/example/stafflist/ui/theme/Color.kt
+++ app/src/main/java/com/example/stafflist/ui/theme/Color.kt
@@ -9,3 +9,12 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//Card Colors
+val FirstLastNameInCardColor = Color(0xFF050510)
+val SpecialityInCardColor = Color(0xFF55555C)
+val TagInCardColor = Color(0xFF97979B)
+//Tab Colors
+val OnSelectTabIndicatorColor = Color(0xFF6534FF)
+val OnSelectTabTextColor = Color(0xFF050510)
+val UnSelectedTabTextColor = Color(0xFF97979B)
