b164cffc61c4424d1c81c22f2af6e7aafd2b7581
Danya Goga <CryingSakura69@gmail.com>
2024-03-10 14:39:24 +0200

    add implementations: Coil, Retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,10 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    //coil for async image
+    implementation("io.coil-kt:coil-compose:2.5.0")
+
+    //Add retrofit2
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
 }
