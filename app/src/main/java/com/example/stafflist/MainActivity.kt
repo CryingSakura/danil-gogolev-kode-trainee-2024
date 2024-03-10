cc2793ac2eb5a935699b4391702b0dcac58d40a3
Danya Goga <CryingSakura69@gmail.com>
2024-03-10 18:12:42 +0200

    create viewModel on MainActivity and call MainView

--- app/src/main/java/com/example/stafflist/MainActivity.kt
+++ app/src/main/java/com/example/stafflist/MainActivity.kt
@@ -3,16 +3,30 @@ package com.example.stafflist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.stafflist.network.RetrofitInstance
+import com.example.stafflist.network.StaffListRepImplementation
+import com.example.stafflist.screen.MainView
+import com.example.stafflist.screen.StaffList.StaffListViewModel
 import com.example.stafflist.ui.theme.StaffListTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<StaffListViewModel>(factoryProducer = {
+        object : ViewModelProvider.Factory{
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return StaffListViewModel(StaffListRepImplementation(RetrofitInstance.requestService)) as T
+            }
+        }
+    })
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +36,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    MainView(viewModel = viewModel)
                 }
             }
         }
