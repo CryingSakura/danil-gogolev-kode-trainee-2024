6f56380c6c09815d8518bf5ae540444abfc0f48a
Danya Goga <CryingSakura69@gmail.com>
2024-03-18 12:06:48 +0200

    change StaffList and StaffListViewModel

--- app/src/main/java/com/example/stafflist/screen/StaffList/StaffList.kt
+++ app/src/main/java/com/example/stafflist/screen/StaffList/StaffList.kt
@@ -1,27 +1,255 @@
 package com.example.stafflist.screen.StaffList
 
+import android.util.Log
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.stafflist.MainActivityViewModel
+import com.example.stafflist.data.DateChanger
 import com.example.stafflist.screen.StaffList.fragments.EmployeeCard
+import com.example.stafflist.screen.StaffList.fragments.NextYearItem
+import kotlinx.coroutines.delay
+import java.util.Calendar
 
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterialApi::class,
+    ExperimentalAnimationApi::class
+)
 @Composable
-fun StaffList(viewModel: StaffListViewModel){
+fun StaffList(viewModel: MainActivityViewModel, navController: NavController){
 
-    val staffList = viewModel.staffList.collectAsState().value
+    val tabRowViewModel = viewModel.topAppBarViewModel.tabRowViewModel
+    val searchViewModel = viewModel.topAppBarViewModel.searchViewModel
+    val staffListViewModel = viewModel.staffListViewModel
+    val sortViewModel = viewModel.sortViewModel
+
+    val searchText = searchViewModel.searchText.collectAsState().value
+
+    val selectedIndex = tabRowViewModel.selectedIndex.collectAsState().value
+
+    val tabListItems = tabRowViewModel.tabTitles.tabListItems
+
+    val staffList = viewModel.staffListViewModel.staffList.collectAsState().value
+
+    val sortId = sortViewModel.indexOfSelectedItem.collectAsState().value
+
+    val calendar = Calendar.getInstance()
+
+    val currentYear = calendar.get(Calendar.YEAR)
+    val nextYear = currentYear + 1
+    val currentMonth = calendar.get(Calendar.MONTH) + 1
+
+    val currentDay = calendar.get(Calendar.DAY_OF_MONTH)
 
 
 
-    Box(modifier = Modifier.fillMaxSize()) {
-        LazyColumn(modifier = Modifier
-            .fillMaxSize()){
-            items(staffList){employee ->
-                EmployeeCard(employee)
-            }
+
+    val isRefreshing = staffListViewModel.isRefreshing.collectAsState().value
+    val refreshing = remember{
+        mutableStateOf(isRefreshing)
+    }
+    val pullRefreshState = rememberPullRefreshState(refreshing = refreshing.value,
+        onRefresh = { staffListViewModel.refresh() },
+        refreshThreshold = 90.dp,)
+
+
+
+
+
+    val pagerState = rememberPagerState{
+        tabListItems.size
+    }
+
+    val loading = remember{
+        mutableStateOf(true)
+    }
+
+
+    LaunchedEffect(key1 = true, block = {
+        delay(10_000)
+        loading.value = false
+    })
+
+
+    val sortedList = when (sortId) {
+        0 -> staffList.sortedBy { it.firstName }
+        1 -> {
+            staffList.sortedBy { DateChanger(it.birthday).day }
+            staffList.sortedBy { DateChanger(it.birthday).month }
+        }
+        else -> {staffList.sortedBy { it.id }}
+    }
+
+    sortedList.forEach {
+        Log.d("GogaMyMonth", "${DateChanger(it.birthday).month}")
+        if (DateChanger(it.birthday).month < currentMonth) {
+            it.dateInNextYear = true
+            Log.e("GogaWork", "$it")
+        }else if (DateChanger(it.birthday).month == currentMonth && DateChanger(it.birthday).day < currentDay){
+            it.dateInNextYear = true
         }
     }
+
+    Log.e("GogaCurrentMonth", "$currentMonth")
+    Log.e("GogaCurrentDay", "$currentDay")
+    Log.e("GogaCurrentYear", "$currentYear")
+
+    var haveBirthdayNextYear = false
+
+    for (i in 0 until sortedList.size){
+        if (sortedList[i].dateInNextYear){
+            haveBirthdayNextYear = true
+            break
+        }
+    }
+    Log.e("GogaHaveBirthday","$haveBirthdayNextYear")
+
+    /*var show: Boolean = false*/
+/*
+    staffListViewModel.sortByIndex(sortId, viewModel.staffListViewModel.staffList)*/
+
+
+    Log.e("GogaSortTestStaff", "${sortViewModel.indexOfSelectedItem.collectAsState().value}")
+    Log.e("GogaSortTestStaff", "${sortViewModel.indexOfSelectedItem}")
+
+
+
+
+
+
+
+
+
+
+
+
+    HorizontalPager(state = pagerState) {
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .pullRefresh(pullRefreshState))
+        {
+
+            Column(modifier = Modifier.fillMaxSize()) {
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    if (!refreshing.value) {
+                        items(items = sortedList.filter {
+
+                            if (tabListItems[selectedIndex] == "All") {
+                                tabListItems[selectedIndex] == "All"
+                            } else {
+                                it.department == tabListItems[selectedIndex]
+                            }
+                        }.filter {
+                            it.firstName.lowercase().startsWith(searchText.lowercase()) ||
+                                    it.lastName.lowercase().startsWith(searchText.lowercase())
+                        }) { employee ->
+                            if (sortId == 0) {
+                                EmployeeCard(employee, viewModel, navController)
+                            }
+
+                            if ((sortId == 1) && (!employee.dateInNextYear)) {
+                                EmployeeCard(employee, viewModel, navController)
+                            }
+                        }
+                        item {
+                            if (sortId == 1 && haveBirthdayNextYear) {
+                                NextYearItem(nextYear = nextYear)
+                            }
+                        }
+                        if (sortId == 1) {
+                            items(items = sortedList.filter {
+                                if (tabListItems[selectedIndex] == "All") {
+                                    tabListItems[selectedIndex] == "All"
+                                } else {
+                                    it.department == tabListItems[selectedIndex]
+                                }
+                            }.filter {
+                                it.firstName.lowercase().startsWith(searchText.lowercase()) ||
+                                        it.lastName.lowercase().startsWith(searchText.lowercase())
+                            }) { employee ->
+                                if (employee.dateInNextYear) {
+                                    EmployeeCard(employee, viewModel, navController)
+                                    Log.e("GogaStaffsBool", "${employee.dateInNextYear}")
+                                    Log.e("GogaStaffsName", "${employee.firstName}")
+                                }
+
+                            }
+                        }
+
+                    }
+                }
+
+                Log.e("GogaSortID", "$sortId")
+
+
+
+                if (sortId == 1) {
+
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxSize()
+                    ) {
+                        if (refreshing.value) {
+                            items(items = sortedList.filter {
+                                if (tabListItems[selectedIndex] == "All") {
+                                    tabListItems[selectedIndex] == "All"
+                                } else {
+                                    it.department == tabListItems[selectedIndex]
+                                }
+                            }.filter {
+                                it.firstName.lowercase().startsWith(searchText.lowercase()) ||
+                                        it.lastName.lowercase().startsWith(searchText.lowercase())
+                            }) { employee ->
+                                if (employee.dateInNextYear) {
+                                    EmployeeCard(employee, viewModel, navController)
+                                    NextYearItem(nextYear = nextYear)
+                                    Log.e("GogaStaffsBool", "${employee.dateInNextYear}")
+                                    Log.e("GogaStaffsName", "${employee.firstName}")
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+
+
+
+
+            PullRefreshIndicator(
+                modifier = Modifier.align(Alignment.TopCenter),
+                refreshing = refreshing.value,
+                state = pullRefreshState,
+                backgroundColor = Color.White)
+
+            }
+
+
+    }
+
+
 }
+
+
--- app/src/main/java/com/example/stafflist/screen/StaffList/StaffListViewModel.kt
+++ app/src/main/java/com/example/stafflist/screen/StaffList/StaffListViewModel.kt
@@ -2,10 +2,12 @@ package com.example.stafflist.screen.StaffList
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.stafflist.data.ChangeDepartmentStringView
 import com.example.stafflist.data.Employee
 import com.example.stafflist.data.StaffListRepository
 import com.example.stafflist.network.Results
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
@@ -16,6 +18,27 @@ import kotlinx.coroutines.launch
 class StaffListViewModel(
     private val staffListRepository: StaffListRepository
 ): ViewModel(){
+
+
+
+    private val _isRefreshing = MutableStateFlow(false)
+    val isRefreshing = _isRefreshing.asStateFlow()
+
+/*
+    fun sortByIndex(index: Int, listForSort: StateFlow<List<Employee>>): List<Employee>{
+        val localList = listForSort.value
+        when(index) {
+            0 -> localList.sortedBy { it.birthday }
+            1 -> localList.sortedBy {
+
+                DateChanger(it.firstName).month
+            }
+        }
+        return localList
+    }
+*/
+
+
     private val _staffList = MutableStateFlow<List<Employee>>(emptyList())
     val staffList =_staffList.asStateFlow()
 
@@ -23,7 +46,16 @@ class StaffListViewModel(
     val showErrorChannel = _showErrorChannel.receiveAsFlow()
 
 
-    init {
+    fun refresh() {
+        viewModelScope.launch {
+            _isRefreshing.value = true
+            delay(3000L)
+            getData()
+            _isRefreshing.value = false
+        }
+    }
+
+     fun getData(){
         viewModelScope.launch {
             staffListRepository.getStaffList().collectLatest {result ->
                 when(result){
@@ -32,11 +64,21 @@ class StaffListViewModel(
                     }
                     is Results.Success -> {
                         result.data?.let {staffs ->
-                            _staffList.update { staffs }
+
+                            _staffList.update { ChangeDepartmentStringView( staffs ) }
                         }
                     }
                 }
             }
         }
+    }
+
+
+
+
+
+
+    init {
+        getData()
     }
 }
