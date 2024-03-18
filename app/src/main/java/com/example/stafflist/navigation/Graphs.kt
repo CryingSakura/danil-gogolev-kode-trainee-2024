46495a330b0b80787769c47596f13eb57c4403eb
Danya Goga <CryingSakura69@gmail.com>
2024-03-18 12:07:09 +0200

    create simple navigation

---
+++ app/src/main/java/com/example/stafflist/navigation/Graphs.kt
@@ -0,0 +1,8 @@
+package com.example.stafflist.navigation
+
+object Graphs {
+    const val ROOT = "root_graph"
+    const val MAINVIEW = "main_view_graph"
+    const val DETAILSCREEN = "detail_screen_graph"
+    const val FATALERROR = "fatal_error_graph"
+}
---
+++ app/src/main/java/com/example/stafflist/navigation/RootNavGraph.kt
@@ -0,0 +1,29 @@
+package com.example.stafflist.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.stafflist.MainActivityViewModel
+import com.example.stafflist.screen.DetailSreen.DetailSreen
+import com.example.stafflist.screen.FatalErrorPage
+import com.example.stafflist.screen.MainView
+
+@Composable
+fun RootNavigationGraph(navController: NavHostController, viewModel: MainActivityViewModel){
+    NavHost(
+        navController = navController,
+        route = Graphs.ROOT,
+        startDestination = Graphs.MAINVIEW){
+        composable(Graphs.MAINVIEW){
+            MainView(viewModel = viewModel, navController)
+        }
+        composable(route = Graphs.DETAILSCREEN){
+            DetailSreen(viewModel = viewModel, navController)
+        }
+        composable(route = Graphs.FATALERROR){
+            FatalErrorPage(navController = navController, viewModel = viewModel)
+        }
+
+    }
+}
