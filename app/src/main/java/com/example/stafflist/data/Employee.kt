64aafeef933ead5fcc353b5fa2e0e276d1307644
Danya Goga <CryingSakura69@gmail.com>
2024-03-18 11:56:22 +0200

    add default parameters to Employee data class

--- app/src/main/java/com/example/stafflist/data/Employee.kt
+++ app/src/main/java/com/example/stafflist/data/Employee.kt
@@ -1,13 +1,14 @@
 package com.example.stafflist.data
 
 data class Employee(
-    val avatarUrl: String,
-    val birthday: String,
-    val department: String,
-    val firstName: String,
-    val id: String,
-    val lastName: String,
-    val phone: String,
-    val position: String,
-    val userTag: String
+    val avatarUrl: String = "",
+    val birthday: String = "",
+    var department: String = "",
+    val firstName: String = "",
+    val id: String = "",
+    val lastName: String = "",
+    val phone: String = "",
+    val position: String = "",
+    val userTag: String = "",
+    var dateInNextYear: Boolean = false
 )
