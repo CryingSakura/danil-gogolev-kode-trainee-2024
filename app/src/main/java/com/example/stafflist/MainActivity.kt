1f22b6d2ae9de729aca2ab75d972b5232e1af033
Danya Goga <CryingSakura69@gmail.com>
2024-03-18 11:59:02 +0200

    add EdgetoEdge, and change Class to creating viewModel

--- app/src/main/java/com/example/stafflist/MainActivity.kt
+++ app/src/main/java/com/example/stafflist/MainActivity.kt
@@ -3,32 +3,55 @@ package com.example.stafflist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.compose.rememberNavController
+import com.example.stafflist.navigation.RootNavigationGraph
 import com.example.stafflist.network.RetrofitInstance
 import com.example.stafflist.network.StaffListRepImplementation
-import com.example.stafflist.screen.MainView
+import com.example.stafflist.screen.CustomTopAppBar.TopAppBarViewModel
+import com.example.stafflist.screen.CustomTopAppBar.fragments.Search.SearchViewModel
+import com.example.stafflist.screen.CustomTopAppBar.fragments.TabRow.TabRowViewModel
+import com.example.stafflist.screen.DetailSreen.DetailSreenViewModel
+import com.example.stafflist.screen.SortScreen.SortViewModel
 import com.example.stafflist.screen.StaffList.StaffListViewModel
 import com.example.stafflist.ui.theme.StaffListTheme
 
 class MainActivity : ComponentActivity() {
 
-    private val viewModel by viewModels<StaffListViewModel>(factoryProducer = {
+    private val viewModel by viewModels<MainActivityViewModel>(factoryProducer = {
         object : ViewModelProvider.Factory{
             override fun <T : ViewModel> create(modelClass: Class<T>): T {
-                return StaffListViewModel(StaffListRepImplementation(RetrofitInstance.requestService)) as T
+                return MainActivityViewModel(
+                    StaffListViewModel(StaffListRepImplementation(RetrofitInstance.requestService)),
+                    TopAppBarViewModel(
+                        TabRowViewModel(),
+                        SearchViewModel()),
+                    DetailSreenViewModel(),
+                    SortViewModel()
+                ) as T
             }
         }
     })
 
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, true)
+
+        enableEdgeToEdge()
         setContent {
             StaffListTheme {
 
@@ -36,7 +59,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainView(viewModel = viewModel)
+                    RootNavigationGraph(navController = rememberNavController(), viewModel = viewModel)
                 }
             }
         }
