9dc1d3aafd05fb3a19945ecf2ff54e0b0d3d8795
Danya Goga <CryingSakura69@gmail.com>
2024-03-20 01:33:48 +0200

    trying change NotFound file

--- app/src/main/java/com/example/stafflist/screen/StaffList/fragments/NotFound.kt
+++ app/src/main/java/com/example/stafflist/screen/StaffList/fragments/NotFound.kt
@@ -2,9 +2,11 @@ package com.example.stafflist.screen.StaffList.fragments
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,28 +16,31 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.stafflist.R
 
 @Composable
 fun NotFound(){
 
-    Column(modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally) {
-        Image(painter = painterResource(id = R.drawable.left_pointing_magnifying_glass),
-            contentDescription = null)
-        
-        Text(modifier = Modifier.fillMaxWidth(),
-            text = stringResource(id = R.string.not_found),
-            fontSize = 17.sp,
-            fontWeight = FontWeight.Bold,
-            color = MaterialTheme.colorScheme.onSecondaryContainer,
-            textAlign = TextAlign.Center)
+        Column(modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(modifier = Modifier.size(56.dp),
+                painter = painterResource(id = R.drawable.left_pointing_magnifying_glass),
+                contentDescription = null)
 
-        Text(text = stringResource(id = R.string.try_correct),
-            fontSize = 16.sp,
-            color = MaterialTheme.colorScheme.surfaceVariant,
-            textAlign = TextAlign.Center)
-    }
+            Text(modifier = Modifier.fillMaxWidth(),
+                text = stringResource(id = R.string.not_found),
+                fontSize = 17.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.onSecondaryContainer,
+                textAlign = TextAlign.Center)
+
+            Text(text = stringResource(id = R.string.try_correct),
+                fontSize = 16.sp,
+                color = MaterialTheme.colorScheme.surfaceVariant,
+                textAlign = TextAlign.Center)
+        }
+
 }
