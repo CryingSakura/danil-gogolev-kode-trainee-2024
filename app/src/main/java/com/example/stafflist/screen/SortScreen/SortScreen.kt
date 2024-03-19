ffd23bea319ca7ab58521d51dadd75f062a98425
Danya Goga <CryingSakura69@gmail.com>
2024-03-19 17:14:17 +0200

    change color

--- app/src/main/java/com/example/stafflist/screen/SortScreen/SortScreen.kt
+++ app/src/main/java/com/example/stafflist/screen/SortScreen/SortScreen.kt
@@ -1,4 +1,4 @@
-import android.util.Log
+
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
@@ -12,13 +12,13 @@ import androidx.compose.material3.SheetState
 import androidx.compose.material3.contentColorFor
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.example.stafflist.MainActivityViewModel
 import com.example.stafflist.screen.SortScreen.fragments.SortElement
 
@@ -58,15 +58,15 @@ fun CustomModalBottomSheet(
     if (showSheet) {
         ModalBottomSheet(
             onDismissRequest = onDismissRequest,
-            modifier = modifier,
+            modifier = modifier.padding(horizontal = 8.dp),
             sheetState = sheetState,
             shape = shape,
-            containerColor = containerColor,
+            containerColor = Color.White,
             contentColor = contentColor,
             tonalElevation = tonalElevation,
             scrimColor = scrimColor,
             dragHandle = dragHandle,
-            windowInsets = windowInsets
+            windowInsets = windowInsets,
         ) {
             Column(modifier = Modifier.padding(bottom = bottomPadding)) {
                 options.forEach {option ->
@@ -74,8 +74,6 @@ fun CustomModalBottomSheet(
                         selectedOption = selectedOption,
                         text = option)
                     if (selectedOption.value == option){
-                        Log.e("GogaSortTest", "${sortViewModel.indexOfSelectedItem.collectAsState().value}")
-                        Log.e("GogaSortTest", "${sortViewModel.indexOfSelectedItem}")
                         sortViewModel.changeIndex(options.indexOf(option))
                     }
                 }
